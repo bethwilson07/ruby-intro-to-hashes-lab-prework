a5f2ed748bdd33c5b9d319b1c791bd7c9595c025
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-09-22 20:20:07 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,9 @@ end
 def monopoly_with_second_tier
   monopoly = {
 	  :railroads => {
-	    :pieces => 4,
-	    :rent_in_dollars => {},
-	    :names => {}
+	   :pieces => 4,
+	   :rent_in_dollars => {},
+	   :names => {}
 	}
 	
 end
@@ -28,16 +28,16 @@ def monopoly_with_third_tier
 	  :railroads => {
 	   :pieces => 4,
 	   :rent_in_dollars => {
-	      :one_piece_owned => 25,
-	      :two_pieces_owned => 50,
-	      :three_pieces_owned => 100,
-	      :four_pieces_owned => 200
+	     :one_piece_owned => 25,
+	     :two_pieces_owned => 50,
+	     :three_pieces_owned => 100,
+	     :four_pieces_owned => 200
 	    },
 	   :names => {
-	      :reading_railroad => {},
-	      :pennsylvania_railroad => {},
-	      :b_and_o_railroad => {},
-	      :shortline => {}
+	     :reading_railroad => {},
+	     :pennsylvania_railroad => {},
+	     :b_and_o_railroad => {},
+	     :shortline => {}
 	   }
 	  }
 	}
@@ -49,22 +49,22 @@ def monopoly_with_fourth_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :rent_in_dollars => {
-	      :one_piece_owned => 25,
-	      :two_pieces_owned => 50,
-	      :three_pieces_owned => 100,
-	      :four_pieces_owned => 200
+	     :one_piece_owned => 25,
+	     :two_pieces_owned => 50,
+	     :three_pieces_owned => 100,
+	     :four_pieces_owned => 200
 	    },
 	    :names => {
-	      :reading_railroad => {
+	     :reading_railroad => {
 	       "mortgage_value" => "$100"
 	     },
-	      :pennsylvania_railroad => {
+	     :pennsylvania_railroad => {
 	       "mortgage_value" => "$200"
 	     },
-	      :b_and_o_railroad => {
+	     :b_and_o_railroad => {
 	       "mortgage_value" => "$400"
 	     },
-	      :shortline => {
+	     :shortline => {
 	       "mortgage_value" => "$800"
 	     }
 	   }
